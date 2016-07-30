e15d3e1976dc7c39a1eef2f7f3b78a0a36a216b8
Gowtham <code@gowtham-raj.com>
2016-07-30 16:45:05 +0530

    list of barber displayed

--- app/js/controllers/manage-barber.js
+++ app/js/controllers/manage-barber.js
@@ -1,4 +1,26 @@
 angular.module('app')
-    .controller('BarberManageController', function ($rootScope, $scope, CompanyModel) {
-    })
-;
+            .controller('BarberManageController', function ($rootScope, $scope, BarberModel) {
+                $scope.allBarbers = [];
+
+
+                    $rootScope.$broadcast('showLoading');
+                    BarberModel.getAllBarbers({
+                    }, getAllBarbersSuccess, getAllBarbersFailure);
+
+
+                function getAllBarbersSuccess(response) {
+                    var barbers = response.data.barbers;
+                    for (var i = 0; i < barbers.length; i++) {
+                        $scope.allBarbers.push(barbers[i]);
+                    }
+
+                    $rootScope.$broadcast('hideLoading');
+                }
+
+                function getAllBarbersFailure() {
+                    $scope.allBarbers = [];
+                }
+
+            })
+        ;
+
--- app/js/models/barber.js
+++ app/js/models/barber.js
@@ -1,4 +1,18 @@
 angular.module('app')
     .service('BarberModel', function ($injector, BASE_URL) {
+        var model = this;
+        var urls = {
+            get_all_barbers: BASE_URL + '/barber/get-all-barber'
+        };
+
+        model.getAllBarbers = function (requestParams, successCallback, failureCallback) {
+            $http = $injector.get('$http');
+            $http.get(urls.get_all_barbers).success(function (response) {
+                console.log(response);
+                successCallback(response);
+            }).error(function (data) {
+                failureCallback(data.error.message)
+            });
+        };
     })
 ;
