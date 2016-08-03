9fb60c5a26c19680a99cb9916bac30ed6a8bd964
Gowtham <code@gowtham-raj.com>
2016-08-03 19:30:24 +0530

    todo update barber

--- app/js/controllers/barber-details.js
+++ app/js/controllers/barber-details.js
@@ -18,8 +18,29 @@ angular.module('app')
         function getBarberFailure() {
             $scope.barber = [];
         }
+
         $scope.getBarberAddress = function (barber) {
             return BarberModel.getBarberAddress(barber);
+        };
+        $scope.updateBarber = function () {
+            BarberModel.updateBarber({
+                id: $stateParams.barberId,
+                first_name: $scope.barber.first_name,
+                last_name: $scope.barber.last_name,
+                email: $scope.barber.email,
+                phone_number: $scope.barber.phone_number,
+                shop_name: $scope.barber.shop_name,
+                address_line_1: $scope.barber.address_line_1,
+                address_line_2: $scope.barber.address_line_2,
+                address_line_3: $scope.barber.address_line_3,
+                logo:$scope.myFile
+            }, updateSuccess, updateFailure)
+        };
+        function updateSuccess(updateResponse) {
+
+        }
+
+        function updateFailure($message) {
         }
 
     });
