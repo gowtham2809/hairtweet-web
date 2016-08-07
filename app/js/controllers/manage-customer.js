93139f3ff58f11d17fa9b14db70df03fa7d87fcb
gowtham raj <gowtham@tortelabs.com>
2016-08-07 22:50:23 +0530

    Fixed bug in customer list

--- app/js/controllers/manage-customer.js
+++ app/js/controllers/manage-customer.js
@@ -2,11 +2,12 @@ angular.module('app')
     .controller('CustomerManageController', function ($rootScope, $scope, CustomerModel) {
         var controller = this;
         $scope.allCustomers = [];
-        $scope.isBusy = false;
+        controller.busy = false;
         controller.page = 1;
         controller.hasMore = true;
+        var tempArray = [];
 
-        $scope.next = function () {
+        $scope.nextPage = function () {
             if (controller.busy || !controller.hasMore)
                 return;
             controller.busy = true;
@@ -19,11 +20,16 @@ angular.module('app')
         function getAllCustomerSuccess(response) {
             var customers = response.users.data;
             for (var i = 0; i < customers.length; i++) {
-                $scope.allCustomers.push(customers[i]);
+                tempArray.push(customers[i]);
+                if (tempArray.length >= 3) {
+                    $scope.allCustomers.push(tempArray);
+                    tempArray = [];
+                }
             }
             controller.page++;
             if (response.users.last_page < controller.page) {
                 controller.hasMore = false;
+                $scope.allCustomers.push(tempArray);
             }
             controller.busy = false;
             $rootScope.$broadcast('hideLoading');
